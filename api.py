a598ba398215a77c42dff9b8249d8fe22bbf9cdc
Lautaro498 <lautarobrancatti03@gmail.com>
2025-06-23 21:01:23 -0300

    metodos de la api

--- api.py
+++ api.py
@@ -1,15 +1,46 @@
 from flask import Flask, request
+from Ahorcado_class import Ahorcado
 
 app = Flask(__name__)
+ahorcado = Ahorcado()
+
+#GET POST PUT DELETE
 
 @app.route('/saludo', methods=['POST'])
 def saludo():
     nombre = request.args.get('nombre', 'desconocido')
     return f'Hola, {nombre}!'
 
-@app.route('/holamundo', methods=['GET'])
-def holamundo():
-    return 'Hola Amigo'
+@app.route('/getRightWord', methods=['GET'])
+def getRightWord():
+    return ahorcado.getRightWord()
+
+
+@app.route('/getWordState', methods=['GET'])
+def getWordState():
+    return ahorcado.getWordState()
+
+
+@app.route('/riskWord', methods=['POST'])
+def riskWord():
+    riskedWord = request.args.get('riskedWord', '')
+    return ahorcado.riskWord(riskedWord)
+
+
+
+@app.route('/riskedLetter', methods=['POST'])
+def riskedLetter():
+    riskedLetter = request.args.get('riskedLetter', '')
+    return ahorcado.riskWord(riskedLetter)
+
+
+
+@app.route('/getRiskedLetters', methods=['GET'])
+def getRiskedLetters():
+    return ahorcado.getRiskedLetters()
+
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
